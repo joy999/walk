285f64c26dace80d495ce2de68be246545d56edb
zhuhua <54zhua@gmail.com>
2014-01-12 02:13:52 +0800

    add walk.SetWindowToDesktopCenter

--- desktop.go
+++ desktop.go
@@ -11,3 +11,9 @@ func GetDesktopSize() Size {
 
 	return Size{int(rect.Right), int(rect.Bottom)}
 }
+
+func SetWindowToDesktopCenter(w Window) {
+	s := GetDesktopSize()
+	w.SetX((s.Width - w.Width()) / 2)
+	w.SetY((s.Height - w.Height()) / 2)
+}
